b831a52d084c6f81d8a32d3a2207c2a6eddf45c2
Patrick Cameron <patcameron@gmail.com>
2015-11-25 16:20:25 -0500

    basic layout and content done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var feed = new Instafeed({
+    get: 'user',
+    accessToken: '416569799.1677ed0.1e2bf2e066ae43068d5157a7014033b2',
+    clientId: '76c3279c838349e98d799054919aa2df',
+    userId: '2040104098',
+    limit: 10
+});
+
+var currentTime = new Date();
+var year = currentTime.getFullYear();
+
+feed.run();
+
+$(function() {
+	$('span.year').text(year);
+	$('.main-gallery').flickity({
+	  contain: true,
+	  wrapAround: true,
+	  pageDots: false
+	});
+});

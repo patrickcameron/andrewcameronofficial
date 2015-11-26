32f6a4cd054dd6e975f3c21c5b882d2e63b40094
Patrick Cameron <patcameron@gmail.com>
2015-11-26 16:29:36 -0500

    video header working

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var feed = new Instafeed({
     accessToken: '416569799.1677ed0.1e2bf2e066ae43068d5157a7014033b2',
     clientId: '76c3279c838349e98d799054919aa2df',
     userId: '2040104098',
-    limit: 10
+    limit: 10,
+    resolution: 'standard_resolution'
 });
 
 var currentTime = new Date();

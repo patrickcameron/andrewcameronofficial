d87d553194a04928a6afb2a4343cfebc564fcdf0
Patrick Cameron <patcameron@gmail.com>
2016-08-01 19:59:41 -0400

    Re-added Press page, changed header font

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 var feed = new Instafeed({
     get: 'user',
-    accessToken: '416569799.1677ed0.1e2bf2e066ae43068d5157a7014033b2',
+    accessToken: '2040104098.1677ed0.45caa4d3a9e349a0897d70909fe0f5f7',
     clientId: '76c3279c838349e98d799054919aa2df',
     userId: '2040104098',
     limit: 10,
-    resolution: 'standard_resolution'
+    resolution: 'standard_resolution',
+    success: function() {
+    	$('section.instagram').show();
+    }
 });
 
 var currentTime = new Date();
